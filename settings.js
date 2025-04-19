2f4019b56d66af1f9044ef914825ecb7920ad4e3
smilebotstore <ttes61665@gmail.com>
2025-04-19 19:23:57 +0800

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,22 @@
+
+const YOUR_MEMID = "#";
+const YOUR_KEYORKUT = "#";
+const YOUR_QR_CODE = "#";
+const domain = 'https://#';
+const apikey = '#';
+const GITHUB_TOKEN = '#';
+const REPO_OWNER = '#';
+const REPO_NAME = '#';
+const FILE_PATH = 'system/panel.json'
+
+module.exports = {
+    YOUR_MEMID,
+    YOUR_KEYORKUT,
+    YOUR_QR_CODE,
+    domain,
+    apikey,
+    GITHUB_TOKEN,
+    REPO_OWNER,
+    REPO_NAME,
+    FILE_PATH
+};
